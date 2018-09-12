f2fe7c6a0f7d55e159234d200a0ab3992355023d
Jérémy Fruit <jeremy.fruit@gmail.com>
2018-09-12 15:52:30 +0200

    ajout de 2 activities

---
+++ app/src/main/java/fr/wildcodeschool/projetmonsterlegend/MonsterExpandedActivity.java
@@ -0,0 +1,13 @@
+package fr.wildcodeschool.projetmonsterlegend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MonsterExpandedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monster_expanded);
+    }
+}
