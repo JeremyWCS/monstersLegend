8a1927703f2a6cf9df05a4d864195903e3c197a6
Edouard Kerhir <edouard.kerhir@gmail.com>
2018-09-13 16:43:38 +0200

    commit de mes travaux sur les stats (page de monstre)

--- app/src/main/java/fr/wildcodeschool/projetmonsterlegend/MainActivity.java
+++ app/src/main/java/fr/wildcodeschool/projetmonsterlegend/MainActivity.java
@@ -1,7 +1,10 @@
 package fr.wildcodeschool.projetmonsterlegend;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+///////////////////////////////////////////////////////////////////////////////////////:
+        Button buttonPage = findViewById(R.id.button_main);
+        final Intent expandedPage = new Intent(MainActivity.this, MonsterExpandedActivity.class);
 
+        buttonPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(expandedPage);
+            }
+        });
     }
 }
