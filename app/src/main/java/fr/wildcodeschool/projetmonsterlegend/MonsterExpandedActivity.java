4ab4b1d1e3823305c367c34df63b53ff8203f039
Edouard Kerhir <edouard.kerhir@gmail.com>
2018-09-13 17:06:40 +0200

    commit suite à légères modifications jeudi 13 septembre, 17:06

--- app/src/main/java/fr/wildcodeschool/projetmonsterlegend/MonsterExpandedActivity.java
+++ app/src/main/java/fr/wildcodeschool/projetmonsterlegend/MonsterExpandedActivity.java
@@ -11,6 +11,7 @@ import android.widget.ImageView;
 import android.widget.NumberPicker;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -41,35 +42,65 @@ public class MonsterExpandedActivity extends AppCompatActivity {
         final int[] force = {142};
         final int[] speed = {24};
         final int[] life = {3};
+
         staminaBar.setMax(200);
         forceBar.setMax(700+142);
         speedBar.setMax(4*70+24);
         lifeBar.setMax(6*70+3);
+
+        //initialisation a la première execution (pour ne pas attendre une modification pour l'affichage de valeurs)
+        lifeText.setText(Integer.toString(life[0]));
+        forceText.setText(Integer.toString(force[0]));
+        speedText.setText(Integer.toString(speed[0]));
+        staminaText.setText(Integer.toString(stamina));
+        staminaBar.setProgress(stamina);
+        forceBar.setProgress(force[0]);
+        speedBar.setProgress(speed[0]);
+        lifeBar.setProgress(life[0]);
+        group.setVisibility(View.GONE);
+
         ok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //je récupère mon string et le reconverti en int
                 String lvlStr=level.getText().toString();
-                int lvlInt=Integer.valueOf(lvlStr);
 
-                //si on passe au dessus du lvl max du monstre on le ramène a son level max
-                if (lvlInt>70) {
-                    level.setText("70");
-                    lvlInt=70;
+                //en cas de non renseignement ou de valeurs négatives
+                if(lvlStr.isEmpty()) {
+                    Toast niveauNok = Toast.makeText(getApplicationContext(), getString(R.string.toast), Toast.LENGTH_LONG);
+                    niveauNok.show();
+                    staminaBar.setProgress(0);
+                    forceBar.setProgress(0);
+                    speedBar.setProgress(0);
+                    lifeBar.setProgress(0);
+                    lifeText.setText(Integer.toString(0));
+                    forceText.setText(Integer.toString(0));
+                    speedText.setText(Integer.toString(0));
+                    staminaText.setText(Integer.toString(0));
+
                 }
 
-                //calcul des stats
-                force[0] = 10*lvlInt+142;
-                speed[0] = 4*lvlInt+24;
-                life[0] =6*lvlInt+3;
-                lifeText.setText(Integer.toString(life[0]));
-                forceText.setText(Integer.toString(force[0]));
-                speedText.setText(Integer.toString(speed[0]));
-                staminaText.setText(Integer.toString(stamina));
-                staminaBar.setProgress(stamina);
-                forceBar.setProgress(force[0]);
-                speedBar.setProgress(speed[0]);
-                lifeBar.setProgress(life[0]);
+                else {
+                    int lvlInt = Integer.valueOf(lvlStr);
+                    //si on passe au dessus du lvl max du monstre on le ramène a son level max
+                    if (lvlInt > 70) {
+                        level.setText("70");
+                        lvlInt = 70;
+                    }
+
+                    //calcul des stats
+                    force[0] = 10 * lvlInt + 142;
+                    speed[0] = 4 * lvlInt + 24;
+                    life[0] = 6 * lvlInt + 3;
+                    lifeText.setText(Integer.toString(life[0]));
+                    forceText.setText(Integer.toString(force[0]));
+                    speedText.setText(Integer.toString(speed[0]));
+                    staminaText.setText(Integer.toString(stamina));
+                    staminaBar.setProgress(stamina);
+                    forceBar.setProgress(force[0]);
+                    speedBar.setProgress(speed[0]);
+                    lifeBar.setProgress(life[0]);
+                }
 
             }
         });
