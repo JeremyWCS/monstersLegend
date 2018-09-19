356eaa8d444bc984129d942d926690d9373d042e
Edouard Kerhir <edouard.kerhir@gmail.com>
2018-09-19 11:25:14 +0200

    Fin de la création de tous les monstres + les stats + les background

--- app/src/main/java/fr/wildcodeschool/projetmonsterlegend/Monster.java
+++ app/src/main/java/fr/wildcodeschool/projetmonsterlegend/Monster.java
@@ -10,11 +10,23 @@ public class Monster {
     private int monster_pics_1;
     private int monster_pics_2;
     private int monster_pics_3;
+    private int habitat;
     private String monster_info;
     private int type;
-    private String[] attaques = new String[9];
+    private String[] attaques;
+    private int aStamina;
+    private int aForce;
+    private int aSpeed;
+    private int aLife;
+    private int bStamina;
+    private int bForce;
+    private int bSpeed;
+    private int bLife;
+    private int[] attakType;
 
-    public Monster(int primaryColor, int secondaryColor, String monster_name, int monster_pics_0, int monster_pics_1, int monster_pics_2, int monster_pics_3, String monsterInfo, int type, String[] attaques){
+
+    public Monster(int primaryColor, int secondaryColor, String monster_name, int monster_pics_0, int monster_pics_1, int monster_pics_2, int monster_pics_3,int habitat, String monsterInfo, int type, String[] attaques, int aStamina, int aForce, int aSpeed, int aLife
+    ,int bStamina, int bForce, int bSpeed, int bLife, int[] attakType){
         this.primaryColor=primaryColor;
         this.secondaryColor=secondaryColor;
         this.monster_name= monster_name;
@@ -22,9 +34,19 @@ public class Monster {
         this.monster_pics_1 = monster_pics_1;
         this.monster_pics_2 = monster_pics_2;
         this.monster_pics_3 = monster_pics_3;
+        this.habitat=habitat;
         this.monster_info=monsterInfo;
         this.type=type;
         this.attaques=attaques;
+        this.aStamina=aStamina;
+        this.aForce=aForce;
+        this.aSpeed=aSpeed;
+        this.aLife=aLife;
+        this.bStamina=bStamina;
+        this.bForce=bForce;
+        this.bSpeed=bSpeed;
+        this.bLife=bLife;
+        this.attakType=attakType;
     }
 
     public String getMonster_name() {
@@ -33,6 +55,14 @@ public class Monster {
 
     public void setMonster_name(String monster_name) {
         this.monster_name = monster_name;
+    }
+
+    public void setHabitat(int habitat) {
+        this.habitat = habitat;
+    }
+
+    public int getHabitat() {
+        return habitat;
     }
 
     public int getMonster_pics_0() {
@@ -112,5 +142,77 @@ public class Monster {
 
     public void setAttaques(String[] attaques) {
         this.attaques = attaques;
+    }
+
+    public int getaForce() {
+        return aForce;
+    }
+
+    public int getaLife() {
+        return aLife;
+    }
+
+    public int getaSpeed() {
+        return aSpeed;
+    }
+
+    public int getaStamina() {
+        return aStamina;
+    }
+
+    public void setaForce(int aForce) {
+        this.aForce = aForce;
+    }
+
+    public void setaLife(int aLife) {
+        this.aLife = aLife;
+    }
+
+    public void setaSpeed(int aSpeed) {
+        this.aSpeed = aSpeed;
+    }
+
+    public void setaStamina(int aStamina) {
+        this.aStamina = aStamina;
+    }
+
+    public int getbForce() {
+        return bForce;
+    }
+
+    public int getbLife() {
+        return bLife;
+    }
+
+    public int getbSpeed() {
+        return bSpeed;
+    }
+
+    public int getbStamina() {
+        return bStamina;
+    }
+
+    public void setbForce(int bForce) {
+        this.bForce = bForce;
+    }
+
+    public void setbLife(int bLife) {
+        this.bLife = bLife;
+    }
+
+    public void setbSpeed(int bSpeed) {
+        this.bSpeed = bSpeed;
+    }
+
+    public void setbStamina(int bStamina) {
+        this.bStamina = bStamina;
+    }
+
+    public int[] getAttakType() {
+        return attakType;
+    }
+
+    public void setAttakType(int[] attakType) {
+        this.attakType = attakType;
     }
 }
--- app/src/main/java/fr/wildcodeschool/projetmonsterlegend/MonstersActivity.java
+++ app/src/main/java/fr/wildcodeschool/projetmonsterlegend/MonstersActivity.java
@@ -31,10 +31,13 @@ public class MonstersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monsters);
-        Intent intent = getIntent();
-        int iMon = intent.getIntExtra("indexMonster",0);
 
-        ArrayList<Monster> monsters=
+        Intent intent = getIntent();
+        final int iMon = intent.getIntExtra("indexMonster",0);
+
+        getSupportActionBar().hide();
+
+        final ArrayList<Monster> monsters=
                 new ArrayList<>();
 
         monsters.add(new Monster(
@@ -45,10 +48,20 @@ public class MonstersActivity extends AppCompatActivity {
                 R.drawable.fire_lion_1,
                 R.drawable.fire_lion_2,
                 R.drawable.fire_lion_3,
+                R.drawable.fire_habitat_8,
                 "Possessing a mane of white-hot hell fire and more than just a sting in his tail – the Fire Lion is a creature of mythic awesomeness! The Fire Lion will make fast work of anyone foolish enough to challenge him.",
                 R.drawable.bte_fire,
-                new String[]{"Surchauffe","Feu Orange","Fourrure Fondate","Griffes Incandescentes","Gazs Enflammés","Peau Eruptive","Feu de Forêt","Lava Trempette","Rayon Solaire Hyper Concentré de la Mort"}
-        ));
+                new String[]{"Surchauffe","Feu Orange","Fourrure Fondate","Griffes Incandescentes","Gazs Enflammés","Peau Eruptive","Feu de Forêt","Lava Trempette","Rayon Solaire Hyper Concentré de la Mort"},
+                0,
+                22,
+                17,
+                177,
+                100,
+                220,
+                170,
+                50,
+                new int[]{R.drawable.bte_fire,R.drawable.bte_fire,R.drawable.bte_fire,R.drawable.bte_fire,R.drawable.bte_fire,R.drawable.bte_fire,R.drawable.bte_fire,R.drawable.bte_fire,R.drawable.bte_fire}
+                ));
 
         monsters.add(new Monster(
                 Color.rgb(140, 26, 255),
@@ -58,9 +71,19 @@ public class MonstersActivity extends AppCompatActivity {
                 R.drawable.genie_1,
                 R.drawable.genie_2,
                 R.drawable.genie_3,
+                R.drawable.magic_habitat_8,
                 "Formed from bright, hot-blue fire and able to travel great distances in the blink of an eye. Think twice before entering into any kind of agreement with these magical marvels.",
                 R.drawable.bte_magic,
-                new String[]{"Attaque Rapide","Coup Etourdi","Rayon Prismatique","Orbe des Arcanes","Surcharge","Attaque Inventée","Fissure des Membres","Nourrir","Magneto"}
+                new String[]{"Attaque Rapide","Coup Etourdi","Rayon Prismatique","Orbe des Arcanes","Surcharge","Attaque Inventée","Fissure des Membres","Nourrir","Magneto"},
+                0,
+                19,
+                25,
+                105,
+                100,
+                190,
+                250,
+                50,
+        new int[]{R.drawable.bte_any,R.drawable.bte_any,R.drawable.bte_magic,R.drawable.bte_magic,R.drawable.bte_magic,R.drawable.bte_any,R.drawable.bte_any,R.drawable.bte_nature,R.drawable.bte_magic}
 
         ));
 
@@ -72,22 +95,171 @@ public class MonstersActivity extends AppCompatActivity {
                 R.drawable.light_spirit_1,
                 R.drawable.light_spirit_2,
                 R.drawable.light_spirit_3,
+                R.drawable.light_habitat_8,
                 "These lighthearted pixie creatures spend most of their time unseen. On account of how they are made from light - they often get mistaken for mirages, yet here they are, right on your island!",
                 R.drawable.bte_light,
-                new String[]{"Piques de Lumière","Coup Herbe","Soin","Frappe Etourdissante","Bouclier","Consecration","Fissure des Membres","Soin Massif","Poings Vampire"}
+                new String[]{"Piques de Lumière","Coup Herbe","Soin","Frappe Etourdissante","Bouclier","Consecration","Fissure des Membres","Soin Massif","Poings Vampire"},
+                0,
+                17,
+                17,
+                149,
+                100,
+                175,
+                175,
+                71,
+                new int[]{R.drawable.bte_light,R.drawable.bte_nature,R.drawable.bte_light,R.drawable.bte_any,R.drawable.bte_any,R.drawable.bte_light,R.drawable.bte_any,R.drawable.bte_light,R.drawable.bte_any}
+
         ));
+
+        monsters.add(new Monster(
+                Color.rgb(135, 135, 135),
+                Color.rgb(230, 230, 230),
+                "Metalsaur",
+                R.drawable.metalsaur_0,
+                R.drawable.metalsaur_1,
+                R.drawable.metalsaur_2,
+                R.drawable.metalsaur_3,
+                R.drawable.metal_habitat_8,
+                "Metal monsters are tough, hard to beat and dazzling. Metalsaur is pure metal, common but deadly. And now with him, a new age of breeding has started! The new Metal element is here!",
+                R.drawable.bte_metal,
+                new String[]{"Charge de Minerai","Violence","Frappe de Fer","Baiser Titane","Compact-Beta","Masse Grandiose","Force Acier","Adamantine-Beta","Masse Divine"},
+                0,
+                22,
+                20,
+                126,
+                100,
+                224,
+                200,
+                60,
+                new int[]{R.drawable.bte_metal,R.drawable.bte_metal,R.drawable.bte_metal,R.drawable.bte_metal,R.drawable.bte_metal,R.drawable.bte_metal,R.drawable.bte_metal,R.drawable.bte_metal,R.drawable.bte_metal}
+
+        ));
+
+        monsters.add(new Monster(
+                Color.rgb(155, 88, 0),
+                Color.rgb(205, 255, 204),
+                "Panda",
+                R.drawable.panda_0,
+                R.drawable.panda_1,
+                R.drawable.panda_2,
+                R.drawable.panda_3,
+                R.drawable.nature_habitat_8,
+                "The Panda, long a symbol of melancholy and sadness, but no longer! These Pandas are Nature's ninjas! What they lack in stealth, they more than make up for in unexpected agility, reflexes and... clumsiness.",
+                R.drawable.bte_nature,
+                new String[]{"Pollen Allergisant","Pause Rafraichissante","Traitement","Poing Organique","Photosynthèse","Troupeau d'Oiseaux en Colère","Biomasse","Ailes Balsamiques","Force de la Nature"},
+                0,
+                19,
+                20,
+                118,
+                100,
+                190,
+                200,
+                56,
+                new int[]{R.drawable.bte_nature,R.drawable.bte_nature,R.drawable.bte_nature,R.drawable.bte_nature,R.drawable.bte_nature,R.drawable.bte_nature,R.drawable.bte_nature,R.drawable.bte_nature,R.drawable.bte_nature}
+
+        ));
+
+        monsters.add(new Monster(
+                Color.rgb(155, 114, 0),
+                Color.rgb(255, 213, 96),
+                "Rockilla",
+                R.drawable.rockilla_0a,
+                R.drawable.rockilla_1a,
+                R.drawable.rockilla_2a,
+                R.drawable.rockilla_3a,
+                R.drawable.earth_habitat_8,
+"These hardened souls roam the earthy plains in search of their tail. They live by a strict code favoring loyalty over all else; any who challenge this are reduced to dust by a swift blow from their gargantuan fists.",
+                R.drawable.bte_earth,
+                new String[]{"Osselets Lourds","Baffe de Boue","Bloc de Pierre","Bâton de Pierre","Dix Tonnes de Sable","Volonté de la Terre","Baffe de Roche","Rockorite","Protection du Maitre King Kong"},
+                0,
+                17,
+                17,
+                149,
+                100,
+                175,
+                175,
+                71,
+                new int[]{R.drawable.bte_earth,R.drawable.bte_earth,R.drawable.bte_earth,R.drawable.bte_earth,R.drawable.bte_earth,R.drawable.bte_earth,R.drawable.bte_earth,R.drawable.bte_earth,R.drawable.bte_earth}
+
+        ));
+
+        monsters.add(new Monster(
+                Color.rgb(239, 242, 53),
+                Color.rgb(142, 255, 240),
+                "Thunder Eagle",
+                R.drawable.thunder_eagle_0,
+                R.drawable.thunder_eagle_1,
+                R.drawable.thunder_eagle_2,
+                R.drawable.thunder_eagle_3,
+                R.drawable.thunder_habitat_8,
+                "The Bringer of Storms, a once-loyal servant to the Gods of Thunder, transformed into Thunder Eagle form after a terrible betrayal. This sparky creature offers the perfect mix of grace and danger.",
+                R.drawable.bte_thunder,
+                new String[]{"Attaque Rapide","Charge Blitzer","Vif Eclair","Fissure de Membres","Faisceau d'Allumage","Orage","Marteau de Thor","Bouclier","Dieu de la Foudre"},
+                0,
+                17,
+                25,
+                105,
+                100,
+                175,
+                250,
+                50,
+                new int[]{R.drawable.bte_any,R.drawable.bte_any,R.drawable.bte_thunder,R.drawable.bte_any,R.drawable.bte_thunder,R.drawable.bte_thunder,R.drawable.bte_thunder,R.drawable.bte_any,R.drawable.bte_thunder}
+
+        ));
+
+        monsters.add(new Monster(
+                Color.rgb(0, 113, 155),
+                Color.rgb(196, 239, 255),
+                "Turtle",
+                R.drawable.turtle_0,
+                R.drawable.turtle_1,
+                R.drawable.turtle_2,
+                R.drawable.turtle_3,
+                R.drawable.water_habitat_8,
+                "Shy and unassuming, this tricky creature is master of the water realm and sharper than shattered glass. The Water Turtle may be slow on land, but he’s as dangerous as he is graceful in the ocean.",
+                R.drawable.bte_water,
+                new String[]{"Fissure de membres","Tourbillon Nettoyant","Attaque Eau","Frappe Superbe","Frappe destabilisante","Attaque Eau","Rigidité Interne","Tsunami","Ouragan"},
+                0,
+                20,
+                20,
+                118,
+                100,
+                200,
+                200,
+                56,
+                new int[]{R.drawable.bte_any,R.drawable.bte_water,R.drawable.bte_water,R.drawable.bte_any,R.drawable.bte_any,R.drawable.bte_water,R.drawable.bte_any,R.drawable.bte_water,R.drawable.bte_water}
+
+        ));
+
+        monsters.add(new Monster(
+                Color.rgb(135, 135, 135),
+                Color.rgb(226, 228, 255),
+                "Tyrannoking",
+                R.drawable.tyrannoking_0,
+                R.drawable.tyrannoking_1,
+                R.drawable.tyrannoking_2,
+                R.drawable.tyrannoking_3,
+                R.drawable.dark_habitat_8,
+"Hail to the king baby! Self-proclaimed Kings of the Lizards, these dark creatures offer the perfect mix of danger, mystery and coolness to any island. Just don’t mention their short arms... these tyrants have got a bite!",
+                R.drawable.bte_dark,
+                new String[]{"Conjonctivite","Nuage Fantôme","Trou Noir","Cauchemar","Apocalypse","Devastation","Fissure de Membres","Poing Vampire","Devorêve"},
+                0,
+                23,
+                17,
+                105,
+                100,
+                230,
+                175,
+                50,
+                new int[]{R.drawable.bte_dark,R.drawable.bte_dark,R.drawable.bte_dark,R.drawable.bte_dark,R.drawable.bte_dark,R.drawable.bte_dark,R.drawable.bte_any,R.drawable.bte_any,R.drawable.bte_dark}
+
+        ));
+
 
         viewPager = findViewById(R.id.viewPager);
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this, monsters.get(iMon));
 
         viewPager.setAdapter(viewPagerAdapter);
-
-
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setTitle("");
-        getSupportActionBar().setLogo(R.drawable.monsterlegends_logo);
-        getSupportActionBar().setDisplayUseLogoEnabled(true);
-
 
 
         final ImageView imageN0 = findViewById(R.id.image_n0);
@@ -138,6 +310,7 @@ public class MonstersActivity extends AppCompatActivity {
         final TextView textAtq9 = findViewById(R.id.textView17);
         final ScrollView scrollView = findViewById(R.id.scrollView);
         final ConstraintLayout corps = findViewById(R.id.constraintLayout);
+        final ImageView habitat = findViewById(R.id.image_arene);
 
 
         monsterName.setText(monsters.get(iMon).getMonster_name());
@@ -148,15 +321,15 @@ public class MonstersActivity extends AppCompatActivity {
         imageN2.setImageResource(monsters.get(iMon).getMonster_pics_2());
         imageN3.setImageResource(monsters.get(iMon).getMonster_pics_3());
         type.setImageResource(monsters.get(iMon).getType());
-        atq1.setImageResource(monsters.get(iMon).getType());
-        atq2.setImageResource(monsters.get(iMon).getType());
-        atq3.setImageResource(monsters.get(iMon).getType());
-        atq4.setImageResource(monsters.get(iMon).getType());
-        atq5.setImageResource(monsters.get(iMon).getType());
-        atq6.setImageResource(monsters.get(iMon).getType());
-        atq7.setImageResource(monsters.get(iMon).getType());
-        atq8.setImageResource(monsters.get(iMon).getType());
-        atq9.setImageResource(monsters.get(iMon).getType());
+        atq1.setImageResource(monsters.get(iMon).getAttakType()[0]);
+        atq2.setImageResource(monsters.get(iMon).getAttakType()[1]);
+        atq3.setImageResource(monsters.get(iMon).getAttakType()[2]);
+        atq4.setImageResource(monsters.get(iMon).getAttakType()[3]);
+        atq5.setImageResource(monsters.get(iMon).getAttakType()[4]);
+        atq6.setImageResource(monsters.get(iMon).getAttakType()[5]);
+        atq7.setImageResource(monsters.get(iMon).getAttakType()[6]);
+        atq8.setImageResource(monsters.get(iMon).getAttakType()[7]);
+        atq9.setImageResource(monsters.get(iMon).getAttakType()[8]);
         infoMonster.setBackgroundColor(monsters.get(iMon).getPrimaryColor());
         skills.setBackgroundColor(monsters.get(iMon).getPrimaryColor());
         stat.setBackgroundColor(monsters.get(iMon).getPrimaryColor());
@@ -175,19 +348,23 @@ public class MonstersActivity extends AppCompatActivity {
         textAtq7.setText(monsters.get(iMon).getAttaques()[6]);
         textAtq8.setText(monsters.get(iMon).getAttaques()[7]);
         textAtq9.setText(monsters.get(iMon).getAttaques()[8]);
+        habitat.setImageResource(monsters.get(iMon).getHabitat());
 
 
 
         //les variables statistiques
-        final int stamina=100;
-        final int[] force = {220};
-        final int[] speed = {175};
-        final int[] life = {50};
+        final int stamina=monsters.get(iMon).getbStamina();
+        final int[] force = {monsters.get(iMon).getbForce()};
+        final int[] speed = {monsters.get(iMon).getbSpeed()};
+        final int[] life = {monsters.get(iMon).getbLife()};
 
-        staminaBar.setMax(200);
-        forceBar.setMax(2000);
-        speedBar.setMax(1700);
-        lifeBar.setMax(8000);
+        int max=0;
+        max=Math.max(monsters.get(iMon).getaForce()*80+monsters.get(iMon).getbForce(),monsters.get(iMon).getaSpeed()*80+monsters.get(iMon).getbSpeed());
+        max=Math.max(max, monsters.get(iMon).getaLife()*80+monsters.get(iMon).getbLife());
+        staminaBar.setMax(monsters.get(iMon).getbStamina()*2);
+        forceBar.setMax(max);
+        speedBar.setMax(max);
+        lifeBar.setMax(max);
 
         //initialisation a la première execution (pour ne pas attendre une modification pour l'affichage de valeurs)
         lifeText.setText(Integer.toString(life[0]));
@@ -366,9 +543,9 @@ public class MonstersActivity extends AppCompatActivity {
                 }
 
                 //calcul des stats
-                force[0] = 22 * lvlInt + 220;
-                speed[0] = 17 * lvlInt + 50;
-                life[0] = 105 * lvlInt + 50;
+                force[0] = monsters.get(iMon).getaForce()*lvlInt+monsters.get(iMon).getbForce();
+                speed[0] = monsters.get(iMon).getaSpeed()*lvlInt+monsters.get(iMon).getbSpeed();
+                life[0] = monsters.get(iMon).getaLife()*lvlInt+monsters.get(iMon).getbLife();
                 lifeText.setText(Integer.toString(life[0]));
                 forceText.setText(Integer.toString(force[0]));
                 speedText.setText(Integer.toString(speed[0]));
